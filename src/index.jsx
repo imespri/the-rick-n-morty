2fd155dd2577cbdb394450678a68557d4bccc5e5
Aleksey <aliaksei.balabushka@gmail.com>
2026-05-03 18:15:45 +0300

    feat: create root constant for ReactDOM

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import "./styles/index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <div>The first React component</div>
   </React.StrictMode>,

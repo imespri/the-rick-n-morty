222a3d8fee604bb1c30b9db9da10939230cc0364
Aleksey <aliaksei.balabushka@gmail.com>
2026-05-03 18:46:15 +0300

    feat: add layout sctructure

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import "./styles/index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Layout } from "./features/layout/layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <div>The first React component</div>
+    <Layout />
   </React.StrictMode>,
 );

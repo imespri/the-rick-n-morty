864faae01eab8b4c9f887a7a7bc1c6ae6d240fb4
Aleksey <aliaksei.balabushka@gmail.com>
2026-05-15 11:27:05 +0300

    refactor: move rating icon styles to RatingRadioGroup

---
+++ src/features/Feedback/FeedbackForm/ReviewSection/RatingRadioGroup/RatingRadioGroup.jsx
@@ -0,0 +1,26 @@
+import "./RatingRadioGroup.scss";
+
+export function RatingRadioGroup() {
+  return (
+    <div className="rating">
+      <label htmlFor="ratingGood">
+        <input
+          id="ratingGood"
+          type="radio"
+          name="rating"
+          value="good"
+          defaultChecked
+        />
+        <span className="rating__icon icon--smile-good" />
+      </label>
+      <label htmlFor="ratingMaybe">
+        <input id="ratingMaybe" type="radio" name="rating" value="maybe" />
+        <span className="rating__icon icon--smile-maybe" />
+      </label>
+      <label htmlFor="ratingBad">
+        <input id="ratingBad" type="radio" name="rating" value="bad" />
+        <span className="rating__icon icon--smile-bad" />
+      </label>
+    </div>
+  );
+}

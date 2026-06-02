7f05a3628462129ed235b543aef830f05331744b
Aleksey <aliaksei.balabushka@gmail.com>
2026-06-02 09:29:41 +0300

    feat: add pagination in Episodes page

--- src/features/Episodes/Episodes.jsx
+++ src/features/Episodes/Episodes.jsx
@@ -1,18 +1,34 @@
 import "./Episodes.scss";
 import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { DataTable } from "@/components/DataTable/DataTable";
+import { Pagination } from "@/components/Pagination/Pagination";
 import { Spinner } from "@/components/Spinner/Spinner";
+import { NotFound } from "@/components/NotFound/NotFound";
 import { fetchEpisodes, fetchCharactersByIds } from "@/services";
 import { getCharacterIds } from "@/utils/getCharacterIds";
 
 export function Episodes() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [episodes, setEpisodes] = useState([]);
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [pageInfo, setPageInfo] = useState(null);
+  const [pageNumber, setPageNumber] = useState(
+    parseInt(searchParams.get("page")) || 1,
+  );
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const loadEpisodes = async () => {
       setLoading(true);
+      setError(null);
+
+      if (pageNumber < 1) {
+        setError("Error");
+        console.error("404 (Not Found)");
+        return;
+      }
 
       try {
         const data = await fetchEpisodes();
@@ -20,8 +36,11 @@ export function Episodes() {
 
         const charactersIds = getCharacterIds(data.results, "characters");
         const charactersData = await fetchCharactersByIds(charactersIds);
+
         setCharacters(charactersData);
+        setPageInfo(data.info);
       } catch (error) {
+        setError(error);
         setEpisodes([]);
       } finally {
         setLoading(false);
@@ -29,7 +48,16 @@ export function Episodes() {
     };
 
     loadEpisodes();
-  }, []);
+  }, [pageNumber]);
+
+  const setPage = (newPage) => {
+    setSearchParams({ page: newPage });
+    setPageNumber(newPage);
+  };
+
+  if (error) {
+    return <NotFound />;
+  }
 
   return (
     <div className="episodes">
@@ -38,11 +66,21 @@ export function Episodes() {
         {loading ? (
           <Spinner />
         ) : (
-          <DataTable
-            page="episodes"
-            data={episodes}
-            allCharacters={characters}
-          />
+          <>
+            <DataTable
+              page="episodes"
+              data={episodes}
+              allCharacters={characters}
+            />
+            {pageInfo && (
+              <Pagination
+                pageInfo={pageInfo}
+                pageNumber={pageNumber}
+                setPageNumber={setPageNumber}
+                setPage={setPage}
+              />
+            )}
+          </>
         )}
       </div>
     </div>
